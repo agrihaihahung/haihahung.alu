3474a2c13162dd2fa4221b25e201be04fb5e34f6
agrihaihahung <agrihaihahung@gmail.com>
2026-01-02 11:16:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000";
+const API = "";
 let allMaterials = [];
 let historyVisible = true;
 
@@ -211,3 +211,4 @@ async function clearData() {
 
   document.getElementById("clearKey").value = "";
 }
+
